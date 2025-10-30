486328fa3597a465d50a5eec0480f8c948cacf21
618989 <poudel.618989@swl.k12.oh.us>
2025-10-30 08:56:02 -0400

    Getting finished

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = InputHelper.getNonZeroLenString(scan, "Enter your name. ");
+        System.out.println(name);
+
+        int faveNum = InputHelper.getInt(scan, "Enter your fav number.");
+        System.out.println(faveNum);
+
+        int numGuess = InputHelper.getRangedInt(scan, "Guess a number between 1-10", 1, 10);
+        System.out.println("You guessed " + numGuess);
+    }
 }
